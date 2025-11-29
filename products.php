3992ada5444a8dca8001454b567209fcab2a52b2
thebeancheese <cruzadayip@gmail.com>
2025-11-29 20:29:30 +0800

    Update products.php

--- products.php
+++ products.php
@@ -1,1 +1,9 @@
+<?php
+  
+$products = [
+    ["name" => "Marlboro", "price" => 180, "stock" => 38],
+    ["name" => "Camel", "price"=> 135, "stock"    => 45],
+    ["name" => "Mighty", "price" => 150, "stock" => 40]
+];
 
+?>
