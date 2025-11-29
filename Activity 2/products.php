d72344634edeec10f6b627863f7c3c93acfa636a
thebeancheese <cruzadayip@gmail.com>
2025-11-29 21:43:01 +0800

    Delete Activity 2 directory

--- Activity 2/products.php
+++
@@ -1,9 +0,0 @@
-<?php
-  
-$products = [
-    ["name" => "Marlboro", "price" => 180, "stock" => 38],
-    ["name" => "Camel", "price"=> 135, "stock"    => 45],
-    ["name" => "Mighty", "price" => 150, "stock" => 40]
-];
-
-?>
