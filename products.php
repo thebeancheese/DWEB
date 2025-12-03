75d7f90e66b7ac3fa999435083a6f1cee08a0978
thebeancheese <cruzadayip@gmail.com>
2025-12-03 21:37:27 +0800

    Update products.php

--- products.php
+++ products.php
@@ -1,9 +1,15 @@
 <?php
-  
-$products = [
-    "Marlboro" =>  ["price" => 180, "stock" => 38],
-    "Camel" =>  ["price"=> 135, "stock"    => 45],
-    "Mighty" =>  ["price" => 150, "stock" => 40]
+declare(strict_types= 1);   
+
+$oyliragis = [
+    "Marlboro" => ["price" => 180, "stock" => 38],
+    "Camel"    => ["price" => 135, "stock" => 45],
+    "Mighty"   => ["price" => 150, "stock" => 40],
+    "Fortune"  => ["price" => 95.00,  "stock" => 22],
+    "Winston"  => ["price" => 170.00, "stock" => 9],
+    "Hope"     => ["price" => 120.00,  "stock" => 12],
+    "Champion" => ["price" => 100.00,  "stock" => 5],
+    "Salem"    => ["price" => 145.00, "stock" => 18],
 ];
 
 ?>
